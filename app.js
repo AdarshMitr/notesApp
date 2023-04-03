d8ad0fa619748ce9eeec3de39a99f08424e97234
AdarshMitr <adarshmitr@gmail.com>
2023-04-03 19:40:24 +0530

    logics for creating and deleting notes

---
+++ app.js
@@ -0,0 +1,24 @@
+const addBtn=document.querySelector('#addBtn')
+const main=document.querySelector('#main')
+
+addBtn.addEventListener('click',
+function(){
+    addNote()
+})
+
+const addNote=()=>{
+    const note=document.createElement('div');
+    note.classList.add('note')
+    note.innerHTML=`
+    <div class="tool">
+      <i class=" save fas fa-save"></i>
+      <i class=" trash fas fa-trash"></i>
+    </div>
+    <textarea></textarea>
+    `;
+    note.querySelector('.trash').addEventListener('click',
+    function(){
+        note.remove()
+    })
+    main.appendChild(note);
+}
