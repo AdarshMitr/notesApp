297a1b65ff64b965b5e1bb3530493ecbbd9ffe4e
AdarshMitr <adarshmitr@gmail.com>
2023-04-04 11:59:37 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,56 +1,54 @@
-const addBtn=document.querySelector('#addBtn')
-const main=document.querySelector('#main')
+const addBtn = document.querySelector("#addBtn");
+const main = document.querySelector("#main");
 
-const saveNotes=()=>{
-    const notes=document.querySelectorAll('.note textarea');
-    const data=[];
-    notes.forEach(
-        (note)=>{
-            data.push(note.value)
-        }
-    )
-    console.log(data)
-    localStorage.setItem('notes',JSON.stringify(data))
-}
-addBtn.addEventListener('click',
-function(){
-    addNote()
+const saveNotes = () => {
+  const notes = document.querySelectorAll(".note textarea");
+  const data = [];
+  notes.forEach((note) => {
+    data.push(note.value);
+  });
+  //   console.log(data);
+  if (data.length == 0) {
+    localStorage.removeItem("notes");
+  } else {
+    localStorage.setItem("notes", JSON.stringify(data));
+  }
+};
+
+addBtn.addEventListener("click", function () {
+  addNote();
 });
 
-
-
-const addNote=(text='')=>{
-    const note=document.createElement('div');
-    note.classList.add('note')
-    note.innerHTML=`
+const addNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
     <div class="tool">
       <i class=" save fas fa-save"></i>
       <i class=" trash fas fa-trash"></i>
     </div>
     <textarea>${text}</textarea>
     `;
-    note.querySelector('.trash').addEventListener('click',
-    function(){
-        note.remove()
-        saveNotes()
-    })
+  note.querySelector(".trash").addEventListener("click", function () {
+    note.remove();
+    saveNotes();
+  });
 
-    note.querySelector('.save').addEventListener('click',
-    function(){
-        saveNotes()
-    })
-    main.appendChild(note);
-    saveNotes()
-}
+  note.querySelector(".save").addEventListener("click", function () {
+    saveNotes();
+  });
+  note.querySelector('textarea').addEventListener("focusout",
+  function(){
+    saveNotes();
+  })
+  main.appendChild(note);
+  saveNotes();
+};
 
-
-(
-    function(){
-const lsNotes=JSON.parse(localStorage.getItem('notes'));
-lsNotes.forEach(
-    (lsNote)=>{
-        addNote(lsNote)
-    }
-)
-    }
-)();
+(function () {
+  const lsNotes = JSON.parse(localStorage.getItem("notes"));
+  
+    lsNotes.forEach((lsNote) => {
+      addNote(lsNote);
+    });
+})();
